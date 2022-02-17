86cb76ebfe885cffbb98f43e82e72d9ac7da7688
MohamadAlammar <99723037+MohamadAlammar@users.noreply.github.com>
2022-02-17 20:26:33 +0200

    Update al-hetafat.page.ts

--- al-hetafat/al-hetafat.page.ts
+++ al-hetafat/al-hetafat.page.ts
@@ -26,7 +26,7 @@ export class AlHetafatPage implements OnInit {
     setTimeout( () => {
       let options: NativeTransitionOptions = {
         direction: 'up',
-        duration: 40,
+        duration: 400,
       }
       this.nativePageTransitions.flip(options);
       this.router.navigate([pageName]);
