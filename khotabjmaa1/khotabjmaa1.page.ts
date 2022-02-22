720496997cbd2348130c40b978c3adf904e446ff
kziad551 <kziad551@gmail.com>
2022-02-22 15:13:41 +0200

    khotabjmaa1

--- khotabjmaa1/khotabjmaa1.page.ts
+++ khotabjmaa1/khotabjmaa1.page.ts
@@ -17,20 +17,65 @@ export class Khotabjmaa1Page implements OnInit {
   
   constructor(private http: HttpClient,private storage: Storage,public navCtrl: NavController,private streamingMedia: StreamingMedia,private router: Router,private popoverCtrl: PopoverController,private popoverController: PopoverController,public loadingController: LoadingController) {
   }
-  ngOnInit(){       const url= 'https://strapi.alsader.net/api/khotab-al-jomaa-docs?filterskhotab-al-jomaa-doc-cats][title][$eq]=khotab-docs&locale=ar-IQ'
-  this.http.get(url).subscribe((res)=>{
-    this.data = res
-    var i =0;
-    for ( i=0; i< this.data.data.length; i++ ) {
-      var array =[];
-      array["title"] = this.data.data[i].attributes.title;
-      array["titleP1"] = this.data.data[i].attributes.titleP1;
-      array["p1"] = this.data.data[i].attributes.p1;
-      array["titleP2"] = this.data.data[i].attributes.titleP2;
-      array["p2"] = this.data.data[i].attributes.p2;
-      this.content.push(array);      
-     }
-  }) }
 
+    ngOnInit() { 
+      this.storage.get('language-usingg').then(linkd => {
+       if( linkd == 'ar'){
+          this.data = [];
+          this.content = [];
+          const url= 'https://strapi.alsader.net/api/khotab-al-jomaa-docs?filters[id][$eq]=1?filters[khotab-al-jomaa-doc-cats][title][$eq]=khotab-docs&locale=ar-IQ'
+          this.http.get(url).subscribe((res)=>{
+            this.data = res
+            var i =0;
+            for ( i=0; i< this.data.data.length; i++ ) {
+              var array =[];
+              array["title"] = this.data.data[i].attributes.title;
+              array["titleP1"] = this.data.data[i].attributes.titleP1;
+              array["p1"] = this.data.data[i].attributes.p1;
+              array["titleP2"] = this.data.data[i].attributes.titleP2;
+              array["p2"] = this.data.data[i].attributes.p2;
+              this.content.push(array);      
+             }
+          }) 
+        }
+        else if( linkd == 'en'){
+          this.data = [];
+          this.content = [];
+          const url= 'https://strapi.alsader.net/api/khotab-al-jomaa-docs?filters[id][$eq]=1?filters[khotab-al-jomaa-doc-cats][title][$eq]=khotab-docs&locale=en'
+          this.http.get(url).subscribe((res)=>{
+            this.data = res
+            var i =0;
+          for ( i=0; i< this.data.data.length; i++ ) {
+            var array =[];
+            array["title"] = this.data.data[i].attributes.title;
+            array["titleP1"] = this.data.data[i].attributes.titleP1;
+            array["p1"] = this.data.data[i].attributes.p1;
+            array["titleP2"] = this.data.data[i].attributes.titleP2;
+            array["p2"] = this.data.data[i].attributes.p2;
+            this.content.push(array);      
+           }
+          }) 
+      }
+        else if( linkd == 'farsi'){
+          this.data = [];
+          this.content = [];
+          const url= 'https://strapi.alsader.net/api/khotab-al-jomaa-docs?filters[id][$eq]=1?filters[khotab-al-jomaa-doc-cats][title][$eq]=khotab-docs&locale=fa-IR'
+          this.http.get(url).subscribe((res)=>{
+            this.data = res
+            var i =0;
+            for ( i=0; i< this.data.data.length; i++ ) {
+              var array =[];
+              array["title"] = this.data.data[i].attributes.title;
+              array["titleP1"] = this.data.data[i].attributes.titleP1;
+              array["p1"] = this.data.data[i].attributes.p1;
+              array["titleP2"] = this.data.data[i].attributes.titleP2;
+              array["p2"] = this.data.data[i].attributes.p2;
+              this.content.push(array);      
+             }
+            }) 
+      }
+    
+  });
+}
     
 }
