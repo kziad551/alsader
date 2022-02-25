540d4c5945c38c1954c9f3539f041ea89e452ced
mhmdkoeik <44830755+mhmdkoeik@users.noreply.github.com>
2022-02-25 11:05:55 +0200

    update

--- masaelshareya/masaelshareya.page.ts
+++ masaelshareya/masaelshareya.page.ts
@@ -3,32 +3,32 @@ import { Router } from '@angular/router';
 import { PopoverController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
-
+import { HttpClient } from "@angular/common/http";
 @Component({
   selector: 'app-masaelshareya',
   templateUrl: './masaelshareya.page.html',
   styleUrls: ['./masaelshareya.page.scss'],
 })
 export class MasaelshareyaPage implements OnInit {
-  public title;
-  public link;
-  public image = [];
+ private data:any = [];
+  public title:any[] = [];
+  public content:any[] = [];
   showcontent: boolean = true;
 
-   constructor(private router: Router,private popoverCtrl: PopoverController,private popoverController: PopoverController,public navCtrl: NavController,private photoViewer: PhotoViewer) {
-       setTimeout( () => {
-     this.title = getXMLDataImage("title");
-     this.link = getXMLDataImage("link");
-     var i =0;
-     for ( i=0; i< this.title.length; i++ ) {
-     var oneimage =[];
-     oneimage["title"] = this.title[i];
-     oneimage["link"] = this.link[i];
-     this.image.push(oneimage);
-    }
-       }, 2000);
+   constructor(private http: HttpClient,private router: Router,private popoverCtrl: PopoverController,private popoverController: PopoverController,public navCtrl: NavController,private photoViewer: PhotoViewer) {
+      
  }
     ngOnInit() {
+      const url ='https://strapi.alsader.net/api/masael-shareya-images?populate=*'
+    this.http.get(url).subscribe((res)=>{
+      this.data = res
+      var i =0;
+      for ( i=0; i< this.data.data.length; i++ ) {
+       var array =[];
+       array["title"] = this.data.data[i].attributes.title;
+       array["link"] = this.data.data[i].attributes.link;
+       this.content.push(array);
+} });
     }
     ionViewWillEnter(){
         let TIME_IN_MS = 2000;
@@ -41,31 +41,31 @@ export class MasaelshareyaPage implements OnInit {
        this.photoViewer.show(url, '', {share: true});
     }
 }
-function getXMLDataImage( itemname:string ) {
+// function getXMLDataImage( itemname:string ) {
 
-  var request = new XMLHttpRequest();
+//   var request = new XMLHttpRequest();
 
-  try {
-          request.open('GET', 'assets/masaelshareya-xml/masaelshareya.xml', false);
-          request.send(null);
-  } catch (err) {
-    return '';
-  }
+//   try {
+//           request.open('GET', 'assets/masaelshareya-xml/masaelshareya.xml', false);
+//           request.send(null);
+//   } catch (err) {
+//     return '';
+//   }
 
-  if (request.status === 200 || request.status === 0) { 
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(request.responseText, "application/xml");
-    var masaael = doc.getElementsByTagName("masala");
-    var result = [];
-    for (var i = 0; i < masaael.length; i++) {
-      var masala = masaael[i];  
+//   if (request.status === 200 || request.status === 0) { 
+//     var parser = new DOMParser();
+//     var doc = parser.parseFromString(request.responseText, "application/xml");
+//     var masaael = doc.getElementsByTagName("masala");
+//     var result = [];
+//     for (var i = 0; i < masaael.length; i++) {
+//       var masala = masaael[i];  
     
-       result.push(masala.getElementsByTagName(itemname)[0].childNodes[0].nodeValue)
-    }
+//        result.push(masala.getElementsByTagName(itemname)[0].childNodes[0].nodeValue)
+//     }
  
-    return result;
-  }
+//     return result;
+//   }
   
-  return '';
+//   return '';
 
-}
+// }
