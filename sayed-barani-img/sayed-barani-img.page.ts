72545f44fdbd6687c23aa8e3606f4a754ed53be3
MohamadAlammar <mohamadammar764@gmail.com>
2022-02-25 11:03:55 +0200

    Done

--- sayed-barani-img/sayed-barani-img.page.ts
+++ sayed-barani-img/sayed-barani-img.page.ts
@@ -3,66 +3,42 @@ import { Router } from '@angular/router';
 import { PopoverController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
-
+import { HttpClient } from "@angular/common/http"; 
 @Component({
   selector: 'app-sayed-barani-img',
   templateUrl: './sayed-barani-img.page.html',
   styleUrls: ['./sayed-barani-img.page.scss'],
 })
 export class SayedBaraniImgPage implements OnInit {
-  ngOnInit() {
-  }
-  public title;
-  public link;
-  public image = [];
- 
- 
-constructor(private router: Router,private popoverCtrl: PopoverController,private popoverController: PopoverController,public navCtrl: NavController,private photoViewer: PhotoViewer) { 
-  
-   this.title = getXMLDataImage("title");
-    this.link = getXMLDataImage("link");
 
-     var i =0;
-     for ( i=0; i< this.title.length; i++ ) {
-     var oneimage =[];
-     oneimage["title"] = this.title[i];
-     oneimage["link"] = this.link[i];
-     this.image.push(oneimage);
+  private data:any = [];
+  public content:any[] = [];
+  constructor(
+    private http: HttpClient,private router: Router,private popoverCtrl: PopoverController,private popoverController: PopoverController,public navCtrl: NavController,private photoViewer: PhotoViewer) { 
     }
- }
 
+  ngOnInit() {
+    const url= 'https://strapi.alsader.net/api/alsuwars?filters[alsuwar_cat][title][$eq]=fi-al-barani&populate=*'
+      this.http.get(url).subscribe((res)=>{
+        this.data = res
+        var i =0;
+        for ( i=0; i< this.data.data.length; i++ ) {
+         var array =[];
+         array["title"] = this.data.data[i].attributes.title;
+         array["link"] = this.data.data[i].attributes.link;
+         this.content.push(array);      
+        }
+      }) 
+ }
+  
+  
+ 
+  
+   
   
 showPhoto(url:string)
 {
    this.photoViewer.show(url, '', {share: true});
 
-}
+}}
 
-}
-function getXMLDataImage( itemname:string ) {
-  var request = new XMLHttpRequest();
-
-  try {
-    request.open('GET', 'assets/images-xml/sayed-fi-al-barani.xml', false);
-    request.send(null); 
-  } catch (err) {  
-    return '';
-  }
-
-  if (request.status === 200 || request.status === 0) { 
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(request.responseText, "application/xml");
-    var images = doc.getElementsByTagName("image");
-    var result = [];
-    for (var i = 0; i < images.length; i++) {
-      var image = images[i];  
-      
-      result.push(image.getElementsByTagName(itemname)[0].childNodes[0].nodeValue)
-    }
- 
-    return result;
-  }
-  
-  return '';
-
-}
