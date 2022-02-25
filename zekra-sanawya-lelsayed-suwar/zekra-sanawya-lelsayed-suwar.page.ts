47752ce4f7e01dcf6fa5f8ed88880945ab9f3724
MohamadAlammar <mohamadammar764@gmail.com>
2022-02-25 16:36:21 +0200

    Done

--- zekra-sanawya-lelsayed-suwar/zekra-sanawya-lelsayed-suwar.page.ts
+++ zekra-sanawya-lelsayed-suwar/zekra-sanawya-lelsayed-suwar.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {NavController, PopoverController} from "@ionic/angular";
 import {PhotoViewer} from "@ionic-native/photo-viewer/ngx";
-
+import { HttpClient } from "@angular/common/http"; 
 @Component({
   selector: 'app-zekra-sanawya-lelsayed-suwar',
   templateUrl: './zekra-sanawya-lelsayed-suwar.page.html',
@@ -10,58 +10,35 @@ import {PhotoViewer} from "@ionic-native/photo-viewer/ngx";
 })
 export class ZekraSanawyaLelsayedSuwarPage implements OnInit {
 
+  private data:any = [];
+  public content:any[] = [];
+  constructor(
+    private http: HttpClient,private router: Router,private popoverCtrl: PopoverController,private popoverController: PopoverController,public navCtrl: NavController,private photoViewer: PhotoViewer) { 
+    }
+
   ngOnInit() {
+    const url= 'https://strapi.alsader.net/api/alzekra-a-lsanawya-images'
+      this.http.get(url).subscribe((res)=>{
+        this.data = res
+        var i =0;
+        for ( i=0; i< this.data.data.length; i++ ) {
+         var array =[];
+         array["title"] = this.data.data[i].attributes.title;
+         array["link"] = this.data.data[i].attributes.link;
+         this.content.push(array);      
+        }
+      }) 
+ }
+  
+
+  
+showPhoto(url:string){
+    var options = {
+      share: true, 
+      closeButton: true, 
+      copyToReference: true 
+    };
+      url = decodeURIComponent(url);
+      this.photoViewer.show(url, '', options);
   }
-  public title;
-  public link;
-  public image = [];
-
-
-  constructor(private router: Router,private popoverCtrl: PopoverController,private popoverController: PopoverController,public navCtrl: NavController,private photoViewer: PhotoViewer) {
-
-    this.title = getXMLDataImage("title");
-    this.link = getXMLDataImage("link");
-
-    var i =0;
-    for ( i=0; i< this.title.length; i++ ) {
-      var oneimage =[];
-      oneimage["title"] = this.title[i];
-      oneimage["link"] = this.link[i];
-      this.image.push(oneimage);
-    }
-  }
-
-
-  showPhoto(url:string)
-  {
-    this.photoViewer.show(url, '', {share: true});
-
-  }
-}
-function getXMLDataImage( itemname:string ) {
-  var request = new XMLHttpRequest();
-
-  try {
-    request.open('GET', 'assets/images-xml/ehyaa-zekraa-eshhad.xml', false);
-    request.send(null);
-  } catch (err) {
-    return '';
-  }
-
-  if (request.status === 200 || request.status === 0) {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(request.responseText, "application/xml");
-    var images = doc.getElementsByTagName("image");
-    var result = [];
-    for (var i = 0; i < images.length; i++) {
-      var image = images[i];
-
-      result.push(image.getElementsByTagName(itemname)[0].childNodes[0].nodeValue)
-    }
-
-    return result;
-  }
-
-  return '';
-
 }
